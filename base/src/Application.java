c6f0e3e5ac88223bef99049ae3c3ab3d8ba604d8
Chaynor Hsiao <chsiao@biointellisense.com>
2021-03-09 20:55:02 -0800

    Add .idea/ and base

---
+++ base/src/Application.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+    }
+}
