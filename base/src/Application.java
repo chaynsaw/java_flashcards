5ddc69f0695915918f99036e416a0cdd338b9be6
Chaynor Hsiao <chsiao@biointellisense.com>
2021-03-10 18:50:00 -0800

    Removing base/ and updating .gitignore to include out/, .DS_Store

--- base/src/Application.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-class Application {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-    }
-}
