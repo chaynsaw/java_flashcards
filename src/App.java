6716a61b4452e06ce1af28b0528fc33b696358dc
Chaynor Hsiao <chaynsaw@gmail.com>
2021-03-12 22:11:08 -0800

    Refactor to play around with hasNextInt()

--- src/App.java
+++ src/App.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        GameChooser gameChooser = new GameChooser();
-        gameChooser.addGame(new NumberGuesser());
-        gameChooser.printAllGames();
-        NumberGuesser game = new NumberGuesser();
-        game.generateRandomInt();
+        int rand = (int) (Math.random() * 20);
+        System.out.println(rand);
+        while (!sc.hasNextInt()) {
+            sc.next();
+            System.out.println("Enter a whole number");
+        }
     }
 }  
